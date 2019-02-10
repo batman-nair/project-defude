21ef609cc1ec7c0c3315421f9f649c0174a79529
haritha1997 <harithapaul97@gmail.com>
2019-02-10 23:31:45 +0530

    Preprocessing added

--- preprocessing.py
+++ preprocessing.py
@@ -6,18 +6,30 @@
 
 import cv2
 
+import numpy as np
+
 def preprocess(img):
-    # Preprocessing steps
-    img = cv2.resize(img, (0, 0), 0, fx = 0.5, fy = 0.5)
+    # Resizing
+    img = cv2.resize(img, (512,512))
+    #Denoising
     img = cv2.fastNlMeansDenoisingColored(img)
-    # img = cv2.equalizeHist(img)
+    #Conversion to Grayscale
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #Sharpening 
+    blur = cv2.GaussianBlur(img,(5,5),0)
+    img = cv2.addWeighted(blur,1.5,img,-0.5,0)
+    #Histogram Equalization
+    equ = cv2.equalizeHist(img)
+    res = np.hstack((img,equ))
+    cv2.imwrite('sample.png',res)
+
 
     return img
 
 
 
 if __name__ == "__main__":
-    img = cv2.imread("./images/doge.jpg");
+    img = cv2.imread("./images/sample.png");
 
     prep_img = preprocess(img)
 

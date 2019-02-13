8ac7a215f9cbf47c8eb059e12218978a8be67c16
haritha1997 <harithapaul97@gmail.com>
2019-02-13 11:13:50 +0530

    final

--- preprocessing.py
+++ preprocessing.py
@@ -5,36 +5,47 @@
 # Output: Preprocessed image for input to the model
 
 import cv2
-
-import numpy as np
+import numpy as np         
+import glob                 # Used for file access
 
 def preprocess(img):
+    # Denoising
+    denoised_img = cv2.fastNlMeansDenoisingColored(img)
+
+    # Conversion to Grayscale
+    gray_img = cv2.cvtColor(denoised_img, cv2.COLOR_BGR2GRAY)
+
+    # Sharpening 
+    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    sharpen_img = cv2.filter2D(gray_img, -1, kernel)
+    
+    # blur = cv2.GaussianBlur(gray_img,(5,5),0)
+    # sharpen_img = cv2.addWeighted(blur,1.5,gray_img,-0.5,0)
+
+    # Histogram Equalization
+    equ_img = cv2.equalizeHist(sharpen_img)
+    
     # Resizing
-    img = cv2.resize(img, (512,512))
-    #Denoising
-    img = cv2.fastNlMeansDenoisingColored(img)
-    #Conversion to Grayscale
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #Sharpening 
-    blur = cv2.GaussianBlur(img,(5,5),0)
-    img = cv2.addWeighted(blur,1.5,img,-0.5,0)
-    #Histogram Equalization
-    equ = cv2.equalizeHist(img)
-    res = np.hstack((img,equ))
-    cv2.imwrite('sample.png',res)
+    resized_img = cv2.resize(equ_img, (512,256))
 
 
-    return img
+    prep_img = resized_img
+    return prep_img
 
 
 
 if __name__ == "__main__":
-    img = cv2.imread("./images/sample.png");
+    dataset_folder = "/home/haritha/Downloads/2011_09_26_drive_0039_sync/2011_09_26/2011_09_26_drive_0039_sync/image_03/data/*.png"
+    for file in glob.glob(dataset_folder):
+        img = cv2.imread(file);
 
-    prep_img = preprocess(img)
+        prep_img = preprocess(img)
 
-    cv2.imshow("Original Image", img)
-    cv2.imshow("Preprocessed Image", prep_img)
+        cv2.imshow("Original Image", img)
+        cv2.imshow("Preprocessed Image", prep_img)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        if cv2.waitKey(2000) == 27: 
+            cv2.destroyAllWindows()
+            break
+        cv2.destroyAllWindows()
+

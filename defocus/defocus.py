357d94c99c7cbaed3436e0790b2a0d8f54c98156
Arjun Nair <arjunnair.mec@gmail.com>
2019-04-02 15:56:33 +0530

    Defocusing applied on each click

--- defocus/defocus.py
+++ defocus/defocus.py
@@ -1,9 +1,10 @@
 import scipy.io as scio
 import numpy as np
 import cv2
+import copy
 
 IMG_DIR = "../images/"
-FILENAME = "sample2"
+FILENAME = "sample0"
 
 class DefocuserObject():
 
@@ -13,7 +14,6 @@ class DefocuserObject():
         self.img = cv2.imread(IMG_DIR + FILENAME + ".png")
         self.blur_imgs = []
 
-        self.first_click = True
 
         self.blur_images()
 
@@ -22,39 +22,33 @@ class DefocuserObject():
         print("Min Depth: ", np.min(self.depth_data))
 
     def normalize_pof(self):
-        self.depth_data = self.depth_data - self.point_of_focus
-        self.depth_data = np.abs(self.depth_data)
-        self.depth_data = self.depth_data / self.depth_data.max()
+        self.norm_depth_data = self.depth_data - self.point_of_focus
+        self.norm_depth_data = np.abs(self.norm_depth_data)
+        self.norm_depth_data = self.norm_depth_data / self.norm_depth_data.max()
 
     # mouse callback function
     def depth_callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
-            if self.first_click:
-                self.first_click = False
-                self.point_of_focus = self.depth_data[y][x]
-                print("Point of focus: ", self.point_of_focus)
-                self.normalize_pof()
-                print("Normalized depth data around point of focus")
+            self.point_of_focus = self.depth_data[y][x]
+            print("Point of focus: ", self.point_of_focus)
+            self.normalize_pof()
+            print("Normalized depth data around point of focus")
 
-                section_size = 1 / len(self.blur_imgs)
-                final_image = np.zeros(self.img.shape)
+            section_size = 1 / len(self.blur_imgs)
+            final_image = np.zeros(self.img.shape)
 
-                print("final shape: ", final_image.shape)
-                print("img shape ", self.img.shape)
+            for index, blur_img in enumerate(self.blur_imgs):
+                mask = (index*section_size <= self.norm_depth_data) & (self.norm_depth_data < (index+1)*section_size)
+                masked_img = copy.deepcopy(blur_img)
+                # Applying mask on copy of blurred image
+                masked_img[mask==0] = [0, 0, 0]
+                final_image = final_image + masked_img
 
-                for index, blur_img in enumerate(self.blur_imgs):
-                    mask = (index*section_size <= self.depth_data) & (self.depth_data < (index+1)*section_size)
-                    masked_img = blur_img
-                    # Applying mask on copy of blurred image
-                    masked_img[mask==0] = [0, 0, 0]
-                    final_image = final_image + masked_img
+            final_image = np.uint8(final_image)
+            cv2.imshow("Final", final_image)
+            cv2.imwrite(IMG_DIR + FILENAME + "_defocus.png", final_image)
 
-                final_image = np.uint8(final_image)
-                cv2.imwrite(IMG_DIR + FILENAME + "_defocus.png", final_image)
-                cv2.imshow("Final", final_image)
-
-            else:
-                print("x: ", x, ", y: ", y, "Depth value: ", self.depth_data[y][x])
+            #     print("x: ", x, ", y: ", y, "Depth value: ", self.norm_depth_data[y][x])
 
     def view_image(self):
         cv2.namedWindow('image')

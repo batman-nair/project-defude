5e97f7aa8e700b05586e43f8061f52660b89c17a
Arjun Nair <arjunnair.mec@gmail.com>
2019-04-02 15:57:52 +0530

    Only denoising and sharpening done

--- preprocessing.py
+++ preprocessing.py
@@ -17,22 +17,22 @@ def preprocess(img):
     denoised_img = cv2.fastNlMeansDenoisingColored(img)
 
     # Conversion to Grayscale
-    gray_img = cv2.cvtColor(denoised_img, cv2.COLOR_BGR2GRAY)
+    # gray_img = cv2.cvtColor(denoised_img, cv2.COLOR_BGR2GRAY)
 
     # Sharpening
     kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-    sharpen_img = cv2.filter2D(gray_img, -1, kernel)
+    sharpen_img = cv2.filter2D(denoised_img, -1, kernel)
 
     # Histogram Equalization
     # equ_img = cv2.equalizeHist(sharpen_img)
     # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    clahe = cv2.createCLAHE()
-    equ_img = clahe.apply(sharpen_img)
+    # clahe = cv2.createCLAHE()
+    # equ_img = clahe.apply(sharpen_img)
 
     # Resizing
-    resized_img = cv2.resize(equ_img, (512,256), cv2.INTER_AREA)
+    # resized_img = cv2.resize(sharpen_img, (512,256), cv2.INTER_AREA)
 
-    prep_img = resized_img
+    prep_img = sharpen_img
     return prep_img
 
 # Run the preprocessing functions on a single image with before and after
@@ -68,6 +68,5 @@ def preprocess_multiple(folder_path):
 
 
 if __name__ == "__main__":
-    dataset_folder = "/home/haritha/Downloads/2011_09_26_drive_0039_sync/2011_09_26/2011_09_26_drive_0039_sync/image_03/data/"
-    preprocess_multiple(dataset_folder)
+    preprocess_single("./images/sample0.png", output="./sample0_pre.png")
 

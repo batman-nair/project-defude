1dd35309ed6a1b31f62b17b5e55508a0ad32a720
Arjun Nair <arjunnair.mec@gmail.com>
2019-04-15 21:22:01 +0530

    Documentation added

--- defocus/defocus.py
+++ defocus/defocus.py
@@ -11,37 +11,51 @@ parser.add_argument('--blur_method', type=str, help='The type of blur to be appl
 
 args = parser.parse_args()
 
-PATH = args.image_path
-FILENAME = os.path.basename(PATH).split('.')[0]
-IMG_DIR = os.path.dirname(PATH)
-BLUR = args.blur_method
-blur_function = {
-        'avg_blur': lambda img,ker_size: cv2.blur(img, (ker_size,ker_size)),
-        'gaussian': lambda img,ker_size: cv2.GaussianBlur(img, (ker_size,ker_size), 0),
-        'median': lambda img,ker_size: cv2.medianBlur(img, ker_size),
-        'bilateral': lambda img,ker_size: cv2.bilateralFilter(img, ker_size, 75, 75),
-        }
-
+# This class holds functions that handle defocusing of an image using a depth map
+# It takes in the path to the image to be defocused and the blur method to be used for defocusing
+# It requires the depth map to be saved in the same folder as the image, with the suffix and extension _disp.npy
+# Parameters:
+#   image_path: Path to the image which is to be defocused
+#   blur_method: The blur function to be used for blurring.
+#       Available values are: gaussian, avg_blur, median, bilateral
 class DefocuserObject():
 
-    def __init__(self):
-        self.depth_data = np.load(os.path.join(IMG_DIR, FILENAME + "_disp.npy"))
-        self.img = cv2.imread(os.path.join(IMG_DIR, FILENAME + ".png"))
+    def __init__(self, image_path = "../images/sample2.png", blur_method = "gaussian"):
+        self.img_name = os.path.basename(image_path).split('.')[0]
+        self.img_dir = os.path.dirname(image_path)
+        self.blur_method = blur_method
+
+        # Lambda functions to call the appropriate blur function according to set method
+        # All the functions takes 2 arguments: the image and the kernel size to be used in the function
+        # As the kernel size increases the amount of blur increases
+        self.blur_function = {
+                'avg_blur': lambda img,ker_size: cv2.blur(img, (ker_size,ker_size)),
+                'gaussian': lambda img,ker_size: cv2.GaussianBlur(img, (ker_size,ker_size), 0),
+                'median': lambda img,ker_size: cv2.medianBlur(img, ker_size),
+                'bilateral': lambda img,ker_size: cv2.bilateralFilter(img, ker_size, 75, 75),
+                }
+
+
+        self.depth_data = np.load(os.path.join(self.img_dir, self.img_name + "_disp.npy"))
+        self.img = cv2.imread(os.path.join(self.img_dir, self.img_name + ".png"))
         self.blur_imgs = []
 
-
+        # The blurred versions of the images can be precalculated
         self.blur_images()
 
-    def view_details(self):
-        print("Max Depth: ", np.max(self.depth_data))
-        print("Min Depth: ", np.min(self.depth_data))
-
+    # Normalizes the depth values based on the depth of focus
+    # The depth of focus is saved in point_of_focus
+    # norm_depth_data holds the final normalized depth of focus
+    # 0 value corresponds to the point which is focused
+    # 1 value corresponds to the point furthest from the point of focus
     def normalize_pof(self):
         self.norm_depth_data = self.depth_data - self.point_of_focus
         self.norm_depth_data = np.abs(self.norm_depth_data)
         self.norm_depth_data = self.norm_depth_data / self.norm_depth_data.max()
 
-    # mouse callback function
+    # Mouse callback function
+    # The point of the click is taken as the point of focus
+    # The depth map is then normalized around the point of focus after which the defocusing of the image is done by sectioning and masking
     def depth_callback(self, event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             self.point_of_focus = self.depth_data[y][x]
@@ -61,10 +75,12 @@ class DefocuserObject():
 
             final_image = np.uint8(final_image)
             cv2.imshow("Final", final_image)
-            cv2.imwrite(os.path.join(IMG_DIR, FILENAME + "_defocus.png"), final_image)
+            cv2.imwrite(os.path.join(self.img_dir, self.img_name + "_defocus.png"), final_image)
 
             #     print("x: ", x, ", y: ", y, "Depth value: ", self.norm_depth_data[y][x])
 
+    # Creates a window to display the original image
+    # The callback function is attached to this window
     def view_image(self):
         cv2.namedWindow('image')
         cv2.setMouseCallback('image', self.depth_callback)
@@ -73,23 +89,13 @@ class DefocuserObject():
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
+    # Generated different blurred versions of the original image
+    # The blurred versions are stored in the list blur_imgs
     def blur_images(self):
         print("Generating blurred versions for image")
         self.blur_imgs.append(self.img)
         for ker_size in range(5, 22, 4):
-            self.blur_imgs.append(blur_function[BLUR](self.img, ker_size))
-
-            # Average blur
-            # self.blur_imgs.append(cv2.blur(self.img, (ker_size,ker_size)))
-
-            # Gaussian blur
-            # self.blur_imgs.append(cv2.GaussianBlur(self.img, (ker_size,ker_size), 0))
-            # Median blur
-            # self.blur_imgs.append(cv2.medianBlur(self.img, ker_size))
-
-            # Bilateral blur
-            # self.blur_imgs.append(cv2.bilateralFilter(self.img, ker_size, 75, 75))
-
+            self.blur_imgs.append(self.blur_function[self.blur_method](self.img, ker_size))
 
         # Show blurred images
         # for index, blur_img in enumerate(self.blur_imgs):
@@ -97,7 +103,8 @@ class DefocuserObject():
 
 
 if __name__ == "__main__":
-    defocuser = DefocuserObject()
+    PATH = args.image_path
+    BLUR = args.blur_method
+
+    defocuser = DefocuserObject(image_path = PATH, blur_method = BLUR)
     defocuser.view_image()
-
-
